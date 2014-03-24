fabb08b12cb9d93c7c149e99819334715c2d7270
robertbl22 <realmadvertising@Roberts-Mac-Pro.local>
2014-03-24 09:26:53 -0400

    Refactored Angular code by feature into folders.
    Added some slight transition animations to people, work, clients. Added
    slight animations to work banners.

--- Gruntfile.js
+++ Gruntfile.js
@@ -55,8 +55,8 @@ module.exports = function (grunt) {
                 port: 9000,
                 livereload: 35729,
                 // change this to '0.0.0.0' to access the server from outside
-                hostname: 'localhost'
-                //hostname: '192.168.1.222' //RWB
+                //hostname: 'localhost'
+                hostname: '192.168.1.222' //RWB
             },
             livereload: {
                 options: {
